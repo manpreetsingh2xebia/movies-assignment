cb63a3522bdf001e773f3a30ecc11c4ca9a9b339
Manpreet Singh <manpreetsingh@countrydelight.in>
2023-03-09 16:32:07 +0530

    initial commit with project setup using hilt and retrofit

---
+++ app/src/main/java/com/xebiaassignment/app/AssignmentApp.kt
@@ -0,0 +1,15 @@
+package com.xebiaassignment.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AssignmentApp : Application() {
+
+    companion object{
+        var appLanguage : String = "en-US"
+    }
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/xebiaassignment/app/modules/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.xebiaassignment.app.modules
+
+import com.xebiaassignment.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(client : OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun loggingInterceptor() : HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkhttp(interceptor: HttpLoggingInterceptor) : OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+
+        if(BuildConfig.DEBUG){
+            okHttpClient.addInterceptor(interceptor)
+        }
+
+        return okHttpClient.build()
+    }
+}
