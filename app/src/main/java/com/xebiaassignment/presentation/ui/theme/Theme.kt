0c51eab9ffe63410b307916206c98e8eea85e051
Manpreet Singh <manpreetsingh@countrydelight.in>
2023-03-10 14:40:39 +0530

    remove dark theme
    remove dark theme check in ui layer because dark theme is not handle in this project.

--- app/src/main/java/com/xebiaassignment/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/xebiaassignment/presentation/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ fun XebiaAssignmentTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
